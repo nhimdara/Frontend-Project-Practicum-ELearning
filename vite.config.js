a2c6983a460e3038ec62bec3c2bd8c1f829d767e
Nhim Dara <daracombodia54@gmail.com>
2026-08-12 19:32:44 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,10 @@ export default defineConfig({
       // Android/Gradle caches can contain tens of thousands of files. They are
       // unrelated to the web app and can exhaust Linux's inotify watch limit.
       ignored: [
-        '**/android/.gradle/**',
-        '**/android/build/**',
-        '**/android/app/build/**',
+        '**/android/**',
+        '**/.gradle/**',
+        '**/dist/**',
+        '**/.git/**',
       ],
     },
   },
